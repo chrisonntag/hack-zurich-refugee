5879f4c3db90218943a67e2b37fbb6694d9db83b
Daryl Rodrigo <darylrodrigo@Daryls-MacBook-Pro.local>
2016-09-17 09:08:21 +0200

    added mongo only for production server

---
+++ server/server/datasources.production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mongoD: {
+    "host": "localhost",
+    "port": 27017,
+    "database": "zurich-hack-refugee",
+    "username": "test",
+    "password": "123",
+    "name": "mongoD",
+    "connector": "mongodb"
+  }
+};
