0a8e9fb70adb1f6dc64fd0aaf0d56fe69df21919
Constantijn Schepens <constantijnschepens@gmail.com>
2016-09-17 19:48:10 +0200

    Pull groups from the backedn

--- server/client/public/js/controllers/survey.controller.js
+++ server/client/public/js/controllers/survey.controller.js
@@ -1,11 +1,17 @@
 angular
 	.module('app')
   .controller('surveyController', function($scope, $rootScope, $state, api) {
-		$scope.survey = {
-			age: "<18",
-			gender: "male",
-			group: "munich",
+		attachResultsToScope = function(data, status, headers, config) {
+			$scope.groups = data
+			$scope.survey = {
+				age: "<18",
+				gender: "male",
+				group: data[0].id.toString()
+			}
+			console.log($scope.survey)
 		}
+
+		api.allGroups(attachResultsToScope)
 
 		$scope.submitSurvey = function() {
 			localStorage.setItem("refufaq_user_details", JSON.stringify($scope.survey))
