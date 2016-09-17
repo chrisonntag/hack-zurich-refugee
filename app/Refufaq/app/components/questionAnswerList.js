45ccb2fbb5c927da090c075ea9ab816ab39cf14e
Constantijn Schepens <constantijnschepens@gmail.com>
2016-09-17 02:06:02 +0200

    Iterate through array of questions

--- app/Refufaq/app/components/questionAnswerList.js
+++ app/Refufaq/app/components/questionAnswerList.js
@@ -6,11 +6,18 @@ import {
 
 import QuestionAnswer from './questionAnswer.js'
 
+let questionsAndAnswers = [
+  {id: 1, question: "question 1", answer: "Answer!"},
+  {id: 2, question: "question 2", answer: "A!"}
+]
+
 export default class QuestionAnswerList extends Component {
   render() {
     return (
       <View>
-        <QuestionAnswer question="This is a question" answer="ANSWER"></QuestionAnswer>
+        { questionsAndAnswers.map(function(questionAndAnswer) {
+          return <QuestionAnswer key={questionAndAnswer.id} question={questionAndAnswer.question} answer={questionAndAnswer.answer}></QuestionAnswer>
+        }) }
       </View>
     )
   }
