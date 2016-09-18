a4236c4a1fd8e8f37e41b9a3ce33b39498ddc4ba
Daryl Rodrigo <darylrodrigo@hotmail.com>
2016-09-18 02:28:41 -0400

    waefaewf

--- server/server/datasources.production.js
+++ server/server/datasources.production.js
@@ -4,7 +4,7 @@ module.exports = {
     "port": 27017,
     "database": "zurich-hack-refugee",
     "username": "test",
-    "password": "123",
+    "password": "73AFWfa39",
     "name": "mongoD",
     "connector": "mongodb"
   }
