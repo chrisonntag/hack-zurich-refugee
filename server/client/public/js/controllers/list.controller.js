7f9e1eb0bffcb22f365e8b511c2bb26019489dc3
Daryl Rodrigo <darylrodrigo@Daryls-MacBook-Pro.local>
2016-09-18 08:45:23 +0200

    removed local storage

--- server/client/public/js/controllers/list.controller.js
+++ server/client/public/js/controllers/list.controller.js
@@ -2,11 +2,10 @@ angular
 .module('app')
   .controller('listController', function($scope, $rootScope, $state, api, surveyService) {
 
-		var groupdId = surveyService.getSurvey().group
-
 		$scope.results = [];
 
 		assignResultsToScope = function(data, status, headers, config) {
+			var groupdId = surveyService.getSurvey().group
 
 			for (var i = 0 ; i < data.length; i++) {
 				if (data[i].groupId == groupId) $scope.results.push(data[i])
